8a8a14c4d503aa984a6cbbcdc534ad9f941bd3c1
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-02-06 00:31:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,51 @@
+require 'pry'
 class Triangle
-  # write code here
+  
+  attr_accessor :one, :two, :three, :type
+  
+  def initialize(one, two, three)
+    
+    @one = one
+    @two = two
+    @three = three
+    
+  end
+  
+  def kind
+    
+    if (@one <=0 || @two <=0 || @three <= 0)
+     
+     binding.pry 
+      # begin
+        raise TriangleError
+      # rescue TriangleError => error
+      #     puts error.message
+      # end
+    
+    else
+      
+      if ( @one == @two && @two == @three)
+         return @type = :equilateral
+         
+      elsif ( @one == @two || @two == @three || @three == @one)
+      
+        return @type = :isosceles
+        
+      else 
+        
+        return @type = :scalene
+        
+      end
+        
+    end
+  
+  end
+  
+  class TriangleError < StandardError
+    # def message 
+    #   "each side must be larger than 0"
+    # end
+  end
+  
+  
 end
