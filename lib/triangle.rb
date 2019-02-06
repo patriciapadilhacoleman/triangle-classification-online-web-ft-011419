f1a1b516bf3f87526d829c78a41d3ae92db92774
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-02-06 00:43:31 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,55 @@
+require 'pry'
 class Triangle
-  # write code here
+  
+  attr_accessor :one, :two, :three, :type
+  
+  def initialize(one, two, three)
+    
+    @one = one
+    @two = two
+    @three = three
+    
+  end
+  
+  def kind
+    
+    if (@one <=0 || @two <=0 || @three <= 0)
+     
+    # binding.pry 
+      # begin
+        raise TriangleError
+      # rescue TriangleError => error
+      #     puts error.message
+      # end
+      
+    elsif (@one + @two <= @three) || (@two + @three <= @one) || (@one + @three <= @two)
+    
+      raise TriangleError
+    
+    else
+      
+      if ( @one == @two && @two == @three)
+         return @type = :equilateral
+         
+      elsif ( @one == @two || @two == @three || @three == @one)
+      
+        return @type = :isosceles
+        
+      else 
+        
+        return @type = :scalene
+        
+      end
+        
+    end
+  
+  end
+  
+  class TriangleError < StandardError
+    # def message 
+    #   "each side must be larger than 0"
+    # end
+  end
+  
+  
 end
